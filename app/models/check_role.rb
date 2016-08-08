425ec35176c9345cbf52cec08e1f51cfb2957b6b
smaram@metanoiasolutions.net <smaram@msspc301.mss.local>
2016-08-08 16:59:20 +0530

    3203: change role id

--- app/models/check_role.rb
+++ app/models/check_role.rb
@@ -3,7 +3,7 @@ class CheckRole
   def check_external_user (projectlist)
 
     projectlist.each do |projectitem|
-      if User.current.roles_for_project(projectitem).include?Role.find_by_id(4)
+      if User.current.roles_for_project(projectitem).include?Role.find_by_id(27)
         return true
       end
     end
