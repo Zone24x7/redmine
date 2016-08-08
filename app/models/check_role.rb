3d305b3c106602ac5096b8a99dc2422f17ef3d4e
smaram@metanoiasolutions.net <smaram@msspc301.mss.local>
2016-08-08 16:56:08 +0530

    3202: adding check role class and change welcome index.html.erb

---
+++ app/models/check_role.rb
@@ -0,0 +1,12 @@
+class CheckRole
+
+  def check_external_user (projectlist)
+
+    projectlist.each do |projectitem|
+      if User.current.roles_for_project(projectitem).include?Role.find_by_id(4)
+        return true
+      end
+    end
+
+  end
+end
