78cd44589f03daf449b56246935f764aa95b6b8a
smaram@metanoiasolutions.net <smaram@msspc301.mss.local>
2016-08-08 17:29:39 +0530

    3206: change in role check method

--- app/models/check_role.rb
+++ app/models/check_role.rb
@@ -1,12 +1,14 @@
 class CheckRole
 
   def check_external_user (projectlist)
-
+    @restult=false
     projectlist.each do |projectitem|
       if User.current.roles_for_project(projectitem).include?Role.find_by_id(27)
-        return true
+        @restult= true
+      else
+        @result=false
       end
     end
-
+    return @restult
   end
 end
