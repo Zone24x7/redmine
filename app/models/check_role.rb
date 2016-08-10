b7a38070b72f767a8931c2da322dfaf29c4bf3c5
smaram@metanoiasolutions.net <smaram@msspc301.mss.local>
2016-08-10 11:51:50 +0530

    3228:check role class method change

--- app/models/check_role.rb
+++ app/models/check_role.rb
@@ -2,15 +2,13 @@ class CheckRole
 
   #check if any project has software- engineer external role
   def check_external_user (projectlist)
-    @restult=false
+    restult=false
     projectlist.each do |projectitem|
       if User.current.roles_for_project(projectitem).include?Role.find_by_id(27)
-        @restult= true
-      else
-        @result=false
+        restult= true
       end
     end
-    return @restult
+    return restult
   end
 
   #check if the given project has a software engineer -external role
